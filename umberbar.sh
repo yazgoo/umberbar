3f276534f6b6969cb013184d74164147fb960a48
yazgoo <yazgoo@gmail.com>
2020-11-17 09:32:15 +0100

    add cpu

--- umberbar.sh
+++ umberbar.sh
@@ -34,6 +34,39 @@ extractmem() {
   echo $(( ( $memtotal - $memfree - $cached - $sreclaimable + $shmem ) * 100 / $memtotal ))
 }
 
+cpu_idle_total() {
+  values=$(cat /proc/stat | head -1 | sed 's/^cpu *//')
+  total=0
+  idle=0
+  i=0
+  for value in $values
+  do
+    if [ $i == 3 ]
+    then
+      idle=$value
+    fi
+    total=$(( total + value ))
+    i=$(( i + 1 ))
+  done
+  echo $idle $total
+}
+
+second() {
+  echo $2
+}
+
+first() {
+  echo $1
+}
+
+cpu_percent() {
+  idle=$(first $1)
+  total=$(second $1)
+  last_idle=$(first $2)
+  last_total=$(second $2)
+  echo $(( 100 - ( (100 * (idle - last_idle) / (total - last_total))) )) 
+}
+
 cols=$(( $COLUMNS / 3 ))
 _2cols=$(( $cols* 2 )) 
 
@@ -43,6 +76,7 @@ then
   battery_path=/sys/class/power_supply/BAT0
 fi
 
+last_idle_total="0 0"
 while true
 do
   battery_capacity=$(colorize $(cat $battery_path/capacity) 255:0:0 20 255:165:0 50 0:165:0)
@@ -57,10 +91,13 @@ do
   fi
   date=$(date)
   temp=$(colorize $[ $(cat /sys/class/thermal/thermal_zone0/temp) / 1000 ] 0:165:0 40 255:165:0 70 255:0:0)
+  idle_total=$(cpu_idle_total)
+  cpu=$(colorize $(cpu_percent "$idle_total" "$last_idle_total") 0:165:0 40 255:165:0 70 255:0:0)
+  last_idle_total="$idle_total"
   windowname=$(xdotool getwindowfocus getwindowname)
   mem=$(colorize $(extractmem) 0:165:0 30 255:165:0 70 255:0:0)
   echo -ne "\033[0;0H"
-  printf "%-${_2cols}.${_2cols}s" "Batt: ${battery_capacity}% $battery_status | Temp: ${temp}C | Mem: ${mem}% | ${windowname}"
+  printf "%-${_2cols}.${_2cols}s" "Batt: ${battery_capacity}% $battery_status | Cpu: ${cpu}% | Temp: ${temp}C | Mem: ${mem}% | ${windowname}"
   echo -ne "\033[0;${_2cols}H"
   printf "%${cols}s" "$date"
   sleep 10
