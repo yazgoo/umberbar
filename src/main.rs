d5ae3c95ede25d1030d68176f78bd6cdcaced9b1
yazgoo <yazgoo@gmail.com>
2021-01-31 11:23:47 +0100

    💻 add CPU

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 extern crate umberbar;
 
 use std::time::Duration;
-use umberbar::{Conf, umberbar, WidgetPosition, Sources, Logos, ThemedWidgets, Palette};
+use umberbar::{Conf, umberbar, WidgetPosition, Sources, Logos, ThemedWidgets, Palette, Widget};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
     let palette = Palette::grey_blue_cold_winter();
-    umberbar(Conf {
-        font: "DroidSansMono Nerd Font".to_string(),
-        font_size: 8,
-        terminal_width: 178,
-        refresh_time: Duration::from_secs(2),
-        widgets: [
-            ThemedWidgets::slash(
+    let mut widgets : HashMap<WidgetPosition, Vec<Widget>> = HashMap::new();
+    let lefts = ThemedWidgets::slash(
                 WidgetPosition::Left, vec![
                 (Sources::battery(), Logos::battery()),
                 (Sources::cpu(), Logos::cpu()),
                 (Sources::cpu_temp(), Logos::cpu_temp()),
                 (Sources::window(), Logos::window()),
-                ], &palette),
-                ThemedWidgets::slash(
+                ], &palette);
+    let rights = ThemedWidgets::slash(
                     WidgetPosition::Right, vec![
                     (Sources::date(), Logos::date()),
                     (Sources::memory(), Logos::memory()),
-                    ], &palette)
-        ].iter().cloned().collect(),
+                    ], &palette);
+    widgets.insert(lefts.0, lefts.1);
+    widgets.insert(rights.0, rights.1);
+    umberbar(Conf {
+        font: "DroidSansMono Nerd Font".to_string(),
+        font_size: 8,
+        terminal_width: 178,
+        refresh_time: Duration::from_secs(2),
+        widgets: widgets,
     }).run().await;
 }
